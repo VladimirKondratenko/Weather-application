6bcd15c0ff59fb8afa4b25179e5fbcde4a3e5cae
KVN <v.n.kondratenko@yandex.by>
2019-06-27 01:20:20 +0300

    add second api

--- src/components/form.js
+++ src/components/form.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Form = props => (
   <form onSubmit={props.weatherMethod}>
+    <input type="radio" value="option1" checked={false} /> BLBALA
+    <input type="radio" value="option1" checked={false}/> BLBALA2
     <input type="text" name="city" placeholder="What is your city?"/>
     <button>Watch the weather</button>
+    
   </form>
 )
 
